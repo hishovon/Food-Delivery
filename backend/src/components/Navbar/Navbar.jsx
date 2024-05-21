66c474ec235cd85419dc83a0bf1d9fb9f8301537
github_username <you@domain.com>
2024-05-22 00:42:07 +0600

    second-commit

--- backend/src/components/Navbar/Navbar.jsx
+++ backend/src/components/Navbar/Navbar.jsx
@@ -1,41 +1,44 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
-const Navbar = () => {
+import { Link } from 'react-router-dom';
 
 
-const [menu,setMenu ] = useState("home");
+const Navbar = ({ setShowLogin }) => {
+
+
+  const [menu, setMenu] = useState("home");
 
 
   return (
     <div className='navbar'>
-        
-     <img src={assets.logo} alt="" className="logo" />
-        
-        <ul className="navbar-menu">
 
-<li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-<li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-<li onClick={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile-app</li>
-<li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact us</li>
+     <Link to={'/'}> <img src={assets.logo} alt="" className="logo" /></Link>
 
-        </ul>
+      <ul className="navbar-menu">
 
-        <div className="navbar-right">
+        <Link to='/' onClick={() => setMenu("home")} className={menu === "home" ? "active" : ""}>Home</Link>
+        <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu === "menu" ? "active" : ""}>Menu</a>
+        <a href='#app-download' onClick={() => setMenu("Mobile-app")} className={menu === "Mobile-app" ? "active" : ""}>Mobile-app</a>
+        <a href='#footer' onClick={() => setMenu("contact-us")} className={menu === "contact-us" ? "active" : ""}>Contact us</a>
 
-<img src={assets.search_icon} alt="" />
-   
-    <div className="navbar-search-icon">
+      </ul>
 
-<img src={assets.basket_icon} alt="" />
-<div className="dot"></div>
+      <div className="navbar-right">
+
+        <img src={assets.search_icon} alt="" />
+
+        <div className="navbar-search-icon">
+
+         <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link> 
+          <div className="dot"></div>
+
+        </div>
+
+        <button onClick={() => setShowLogin(true)}>Sigh in</button>
+      </div>
 
     </div>
-
-    <button>Sigh in</button>
-        </div>
-        
-        </div>
   )
 }
 
--- backend/src/context/StoreContext.jsx
+++ backend/src/context/StoreContext.jsx
@@ -2,60 +2,78 @@ import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 
-export const StoreContext = createContext(null) 
+export const StoreContext = createContext(null)
 
-const StoreContextProvide =(props)=>{
+const StoreContextProvide = (props) => {
 
-const [cartItems,setCartItems] = useState({});
+    const [cartItems, setCartItems] = useState({});
 
-const addToCart = (itemId) => {
+    const addToCart = (itemId) => {
 
-if(!cartItems[itemId]){
+        if (!cartItems[itemId]) {
 
-    setCartItems((prev)=>({...prev,[itemId]:1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
 
-}
+        }
 
-else {
+        else {
 
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}))
-}
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
+        }
 
-}
+    }
 
-const removeFromCart = (itemId) =>{
+    const removeFromCart = (itemId) => {
 
-setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
 
-}
-
-useEffect(()=>{
-
-console.log();
+    }
 
 
-},[cartItems])
-
-const contextValue ={
-
-food_list,
-cartItems,
-setCartItems,
-addToCart,
-removeFromCart
-
-}
-
-return (
+    const getTotalCartAmount = () => {
 
 
-<StoreContext.Provider value={contextValue}>
+        let totalAmount = 0;
 
-{props.children}
+        for (const item in cartItems) {
 
-</StoreContext.Provider>
+            if (cartItems[item] > 0) {
 
-)
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+
+            }
+
+
+
+        }
+
+        return totalAmount;
+
+    }
+
+
+    const contextValue = {
+
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart,
+        getTotalCartAmount
+
+    }
+
+    return (
+
+
+        <StoreContext.Provider value={contextValue}>
+
+            {props.children}
+
+        </StoreContext.Provider>
+
+    )
 
 }
 
